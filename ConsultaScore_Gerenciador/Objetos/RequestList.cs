a3ba3c43b68cb72427671bbb6f421fad1881f569
K1t4r0 <k1t4r00@hotmail.com>
2014-09-18 17:54:54 -0300

    Comitando

---
+++ ConsultaScore_Gerenciador/Objetos/RequestList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaScore_Gerenciador.Objetos
+{
+    class RequestList
+    {
+        public int ID { get; set; }
+        public int CPF { get; set; }
+
+        public RequestList()
+        {
+
+        }
+        public RequestList(int ID, int CPF)
+        {
+            this.ID  = ID;
+            this.CPF = CPF;
+        }
+
+
+    }
+}
